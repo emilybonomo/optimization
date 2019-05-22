ef236b5c3652f3cb0c815cfbe2537ea1d1a866b3
Emily <emily.bonomo@mail.mcgill.ca>
2019-05-22 11:13:17 -0400

    Added loop with steepest descent method

--- steepest_descent1.m
+++ steepest_descent1.m
@@ -11,3 +11,13 @@ x1 = x0 - alpha*gradf;
 iterations = 1;
 err = norm(gradf);
 error_plot(iterations) = err;
+
+while err > 10^-5
+	iterations = iterations + 1;
+	x0 = x1;
+	gradf(1,1) = -2*(1-x0(1,1)) - 400*x0(1,1)*(x0(2,1)-x0(1,1)^2);
+    gradf(2,1) = 200*(x0(2,1)-x0(1,1)^2);
+    x1 = x0 - alpha*gradf;
+    err = norm(gradf);
+    error_plot(iterations)=err;
+end
